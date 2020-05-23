7c2149b57ea46b5359bd2959ec5d900a0ec5dd45
MelanX <info@melanx.de>
2020-05-23 22:22:01 +0200

    spawneggs spawn entities now

--- src/main/java/de/melanx/invswitch/items/ItemLootBox.java
+++ src/main/java/de/melanx/invswitch/items/ItemLootBox.java
@@ -1,10 +1,13 @@
 package de.melanx.invswitch.items;
 
 import de.melanx.invswitch.InventorySwitch;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.SpawnEggItem;
 import net.minecraft.util.*;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
@@ -20,7 +23,7 @@ public class ItemLootBox extends Item {
     public static final ResourceLocation loot_table = new ResourceLocation(InventorySwitch.MODID, "lootbox");
 
     public ItemLootBox(Properties properties) {
-        super(properties);
+        super(properties.maxStackSize(1));
     }
 
     @Override
@@ -30,7 +33,11 @@ public class ItemLootBox extends Item {
             LootContext.Builder builder = new LootContext.Builder((ServerWorld) worldIn).withParameter(LootParameters.TOOL, playerIn.getHeldItem(handIn)).withParameter(LootParameters.POSITION, playerIn.getPosition());
             List<ItemStack> items = table.generate(builder.build(LootParameterSets.FISHING));
             for (ItemStack item : items) {
-                if (!playerIn.inventory.addItemStackToInventory(item)) {
+                if (item.getItem() instanceof SpawnEggItem) {
+                    EntityType<?> entityType = ((SpawnEggItem) item.getItem()).getType(item.getTag());
+                    for (int i = 0; i < item.getCount(); i++)
+                        entityType.spawn(worldIn, item, playerIn, playerIn.getPosition(), SpawnReason.SPAWN_EGG, false, false);
+                } else if (!playerIn.inventory.addItemStackToInventory(item)) {
                     worldIn.addEntity(new ItemEntity(worldIn, playerIn.posX, playerIn.posY, playerIn.posZ, item));
                 }
             }
